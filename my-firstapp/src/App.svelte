051c7ddd5138490e633ab61b46111a77992d550f
Alexis Loza <arquidec@gmail.comn>
2020-06-17 16:53:37 -0300

    Statrting App Svelte

---
+++ my-firstapp/src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+  export let name, age;
+</script>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #0511ac;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <h3>Your Age is:{age}!</h3>
+  <p>
+    Visit the
+    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    to learn how to build Svelte apps.
+  </p>
+</main>
