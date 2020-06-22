4c73cbf069c6b347575f3799105a98e74ad2cf9b
Alexis Loza <arquidec@gmail.comn>
2020-06-22 20:21:00 -0300

    api working

--- my-firstapp/src/App.svelte
+++ my-firstapp/src/App.svelte
@@ -1,33 +1,81 @@
 <script>
   import MovieItem from "./Movie/Item.svelte";
-  import { onMount } from "svelte";
+  // import { onMount } from "svelte";
+  import { fly } from "svelte/transition";
 
   const APIKEY = "4d34fed296059cb1dd1ba88983ebbd3e";
   const BASEURL = `https://api.themoviedb.org/3`;
   const APISETTINGS = `?api_key=${APIKEY}&language=es-MX`;
-  let movies = [];
 
-  function fetchMovies() {
+  // function fetchMovies() {
+  //   const URL = `${BASEURL}/discover/movie${APISETTINGS}&sort_by=popularity.desc`;
+
+  //   fetch(URL)
+  //     .then(res => res.json())
+  //     .then(({ results }) => {
+  //       movies = results;
+  //       console.log(results);
+  //     });
+  // }
+
+  const movies = (async () => {
     const URL = `${BASEURL}/discover/movie${APISETTINGS}&sort_by=popularity.desc`;
+    const response = await fetch(URL);
 
-    fetch(URL)
-      .then(res => res.json())
-      .then(({ results }) => {
-        movies = results;
-        console.log(results);
-      });
-  }
+    return await response.json();
+  })();
 
-  onMount(() => {
-    console.log("the component has mounted");
-    fetchMovies();
-  });
+  // onMount(() => {
+  //   console.log("the component has mounted");
+  //   fetchMovies();
+  // });
+
+  let likedMovies = [];
+
+  const toogleLike = (event) => {
+    console.log(event);
+    const movie = event.detail;
+
+    let index = likedMovies.findIndex((m) => m.id === movie.id);
+
+    if (index >= 0) {
+      likedMovies.splice(index, 1);
+      console.log(likedMovies);
+      likedMovies = likedMovies;
+      return;
+    }
+    likedMovies.push(movie);
+    likedMovies = likedMovies;
+    console.log(likedMovies);
+  };
+
+  $: like = (id) => {
+    let index = likedMovies.findIndex((m) => m.id === id);
+    return index >= 0;
+  };
 </script>
 
 <style>
+  .panel {
+    height: 100vh;
+    overflow: auto;
+  }
+  main {
+    text-align: center;
+    padding: 0;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+  h1 {
+    color: #3431f5;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
   @media (min-width: 640px) {
     main {
-      max-width: 100vh;
+      max-width: 100vw;
+      background-color: rgb(226, 226, 226);
     }
   }
 </style>
@@ -43,7 +91,9 @@
 
 <main class="container">
   <div class="row">
-    {#each movies as movie}
+    <div class="col-12 col-md-6 col-lg-8 border panel">
+      <h1>Popular Movies</h1>
+      <!-- {#each movies as movie}
       <div class="col-12 col-md-6 col-lg-3 p-2">
         <MovieItem
           id={movie.id}
@@ -52,8 +102,59 @@
           release_date={movie.release_date}
           cover={movie.poster_path} />
       </div>
-    {/each}
+    {/each} -->
+      <div class="row">
+        {#await movies}
+          <!-- promise is pending -->
+          <p>Loading...</p>
+        {:then data}
+          <!-- promise was fulfilled -->
+          {#each data.results as movie}
+            <div class="col-12 col-md-6 col-lg-4 p-1">
+              <MovieItem
+                like={like(movie.id)}
+                title={movie.title}
+                overview={movie.overview}
+                release_date={movie.release_date}
+                cover={movie.poster_path}
+                on:onToogleLike={toogleLike} />
+            </div>
+          {/each}
+        {/await}
+      </div>
+
+    </div>
+    <div class="col-12 col-md-6 col-lg-4 border panel">
+      <h2>Favorites Movies</h2>
+      <div class="row">
+
+        {#if likedMovies.length}
+          {#each likedMovies as movie, i (movie.id)}
+            <div
+              in:fly={{ duration: 200, y: 20 }}
+              out:fly={{ duration: 800, y: -20 }}
+              class="col-12 col-md-6 col-lg-4 p-2">
+              <MovieItem
+                like={like(movie.id)}
+                title={movie.title}
+                overview=""
+                release_date={movie.release_date}
+                cover={movie.cover}
+                on:onToogleLike={toogleLike} />
+            </div>
+          {/each}
+        {:else}
+          <div class="col-12">
+            <p>No Movies Favorites</p>
+          </div>
+        {/if}
+      </div>
+    </div>
 
   </div>
+  <footer>
+    Powered by
+    <a href="https://alexisdev.co" target="_blank">Alexis Loza</a>
+  </footer>
 
 </main>
--- my-firstapp/src/Movie/Item.svelte
+++ my-firstapp/src/Movie/Item.svelte
@@ -1,21 +1,45 @@
 <script>
+  import { createEventDispatcher } from "svelte";
+
   export let id;
   export let title;
   export let release_date;
   export let overview;
   export let cover;
+  export let like;
 
   $: coverURL = `https://image.tmdb.org/t/p/w185_and_h278_bestv2${cover}`;
+
+  const dispatch = createEventDispatcher();
+
+  const toogleLike = () => {
+    dispatch("onToogleLike", {
+      id,
+      title,
+      overview,
+      release_date,
+      cover,
+      like,
+    });
+  };
 </script>
 
 <div class="card">
-  <img src={coverURL} class="card-img-top" alt="..." />
+  <img src={coverURL} class="card-img-top" alt="Movie " />
   <div class="card-body">
     <h5 class="card-title">{title}</h5>
-    <p class="card-text">{overview}</p>
+    <h6 class="card-text">ID:{id}</h6>
+    <p class="card-text">
+      <span class="font-weight-bold">Description Movie:</span>
+      {overview}
+    </p>
     <p class="card-text">Start Date.{release_date}</p>
     <div class="text-center">
-      <button class="btn btn-primary">Like</button>
+      <button
+        class={like ? 'btn btn-secondary' : 'btn btn-primary'}
+        on:click={toogleLike}>
+        {like ? 'Unlike' : 'Like'}
+      </button>
     </div>
   </div>
 </div>
